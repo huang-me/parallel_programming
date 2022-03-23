1f83146d17d4b96111d3d24ea4885e1ee1adb7b0
huang-me <amos0107@gmail.com>
2022-03-23 15:04:06 +0800

    Calculate pi in serial

---
+++ pp_hw2/part1/serial.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+#define ran \
+    -1 + static_cast<float> (rand()) / static_cast<float> (RAND_MAX / 2)
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    int number_in_circle = 0;
+    int number_of_tosses = atoi(argv[1]);
+    for (int toss = 0; toss < number_of_tosses; toss++) {
+        double x = ran, y = ran;
+        double distance_squared = x * x + y * y;
+        if ( distance_squared <= 1)
+            number_in_circle++;
+    }
+    float pi_estimate = 4 * number_in_circle /(( double ) number_of_tosses);
+    cout << pi_estimate << endl;
+    return 0;
+}
