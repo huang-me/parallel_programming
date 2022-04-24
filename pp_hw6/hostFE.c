022ef0d48ce970b9e99021502552dc44163ba73f
huang-me <amos0107@gmail.com>
2022-04-24 22:43:50 +0800

    Basic OpenCL code run convolution

--- pp_hw6/hostFE.c
+++ pp_hw6/hostFE.c
@@ -9,5 +9,35 @@ void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
 {
     cl_int status;
     int filterSize = filterWidth * filterWidth;
+	
+	int err;
+	cl_kernel kernel = clCreateKernel(*program, "convolution", &err);
 
+	cl_mem input, output, filterKernel;
+	input = clCreateBuffer(*context, CL_MEM_READ_WRITE, sizeof(float) * imageWidth * imageHeight, NULL, NULL);
+	output = clCreateBuffer(*context, CL_MEM_READ_WRITE, sizeof(float) * imageWidth * imageHeight, NULL, NULL);
+	filterKernel = clCreateBuffer(*context, CL_MEM_READ_WRITE, sizeof(float) * filterSize, NULL, NULL);
+
+	cl_command_queue commands = clCreateCommandQueue(*context, *device, 0, &err);
+	clEnqueueWriteBuffer(commands, input, CL_TRUE, 0, sizeof(float) * imageWidth * imageHeight, inputImage, 0, NULL, NULL);
+	clEnqueueWriteBuffer(commands, filterKernel, CL_TRUE, 0, sizeof(float) * filterSize, filter, 0, NULL, NULL);
+
+	clSetKernelArg(kernel, 0, sizeof(cl_mem), (void**) &input);
+	clSetKernelArg(kernel, 1, sizeof(cl_mem), (void**) &filterKernel);
+	clSetKernelArg(kernel, 2, sizeof(cl_mem), (void**) &output);
+	clSetKernelArg(kernel, 3, sizeof(int), &filterWidth);
+	clSetKernelArg(kernel, 4, sizeof(int), &imageWidth);
+	clSetKernelArg(kernel, 5, sizeof(int), &imageHeight);
+
+	size_t global[2] = {imageWidth, imageHeight}, local[2] = {1, 1};
+	clEnqueueNDRangeKernel(commands, kernel, 2, NULL, global, local, 0, NULL, NULL);
+
+	clFinish(commands);
+	clEnqueueReadBuffer(commands, output, CL_TRUE, 0, sizeof(float) * imageWidth * imageHeight, outputImage, 0, NULL, NULL);
+
+
+	clReleaseMemObject(input);
+	clReleaseMemObject(output);
+	clReleaseMemObject(filterKernel);
+	clReleaseCommandQueue(commands);
 }
--- pp_hw6/kernel.cl
+++ pp_hw6/kernel.cl
@@ -1,4 +1,20 @@
-__kernel void convolution() 
+__kernel void convolution(	__global float *input, \
+							__global float *filter, \
+							__global float *output, \
+							int filterWidth, \
+							int imgWidth, \
+							int imgHeight)
 {
-   
+	int x = get_global_id(0), y = get_global_id(1);
+	int hf = filterWidth / 2, mid = hf + 1;
+	float curr = 0.f;
+	for(int i = -1 * hf; i <= hf; i++) {
+		for(int j = -1 * hf; j <= hf; j++) {
+			if(i + x >= 0 && i + x < imgWidth && j + y >= 0 && j + y < imgHeight) {
+				curr += input[i + x + (j + y) * imgWidth] * filter[i + hf + (j + hf) * filterWidth];
+			}
+		}
+	}
+	output[x + y * imgWidth] = curr;
+	return;   
 }
